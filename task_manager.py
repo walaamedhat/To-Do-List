f9c45c50c9d5b47f8cd7b029fdf9375aa953f153
Walaa <walaa@Walaas-MacBook-Air.local>
2025-07-13 18:24:50 +0300

    initial setup and added TODO instructions

---
+++ task_manager.py
@@ -0,0 +1,40 @@
+# task_manager.py
+
+# ToDo List to keep track of task and should be dictionary
+# Ex: [{"task_id": 1, "title": "Hit the gym", "priority": "High", "status": "To Do" }]
+# Task_id => Number
+# Title => String
+# Priority => String ("High" / "Medium" / "Low")
+# Status => String ("To Do" / "Doing" / "Done")
+# Description => String
+
+tasks = []
+
+# ToDo => Function to add a new task, task will be a dictionary
+def add_task(task):
+    # write your code here
+    pass
+
+
+# ToDo => Function to remove a task by ID
+def remove_task(task_id):
+    # write your code here
+    pass
+
+
+# ToDo => Function to mark a task as completed
+def complete_task(task_id):
+    # write your code here
+    pass
+
+
+# ToDo => Function to list all tasks
+def list_tasks():
+    # write your code here
+    pass
+
+
+# ToDo => Function to edit a task's description
+def edit_task(task_id, new_description):
+    # write your code here
+    pass

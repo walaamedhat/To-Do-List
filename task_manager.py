12b3882d44189219d362dc9e2867974ee240d78a
Walaa <walaa@Walaas-MacBook-Air.local>
2025-07-26 19:15:57 +0300

    replace program from globals to class

--- task_manager.py
+++ task_manager.py
@@ -1,40 +1,59 @@
 # task_manager.py
 
 # ToDo List to keep track of task and should be dictionary
-# Ex: [{"task_id": 1, "title": "Hit the gym", "priority": "High", "status": "To Do" }]
+# Ex: [{"task_id": 1, "title": "AUC CLass", "priority": "High", "status": "To Do", "description": "Python Class" }]
 # Task_id => Number
 # Title => String
 # Priority => String ("High" / "Medium" / "Low")
 # Status => String ("To Do" / "Doing" / "Done")
 # Description => String
 
-tasks = []
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+        self._next_id = 1
 
-# ToDo => Function to add a new task, task will be a dictionary
-def add_task(task):
-    # write your code here
-    pass
+    # ToDo => Function to validate task, task will be a dictionary
+    def validation(self, task_input):
+        pass
+
+    # ToDo => Function to add a new task, task will be a dictionary
+    def add_task(self, task_data):
+        # write your code here
+        task = {
+            "id": self._next_id,
+            **task_data
+        }
+        self.tasks.append(task)
+        self._next_id += 1
+        # pass
+
+    def get_tasks(self):
+        return self.tasks
+
+    def clear_tasks(self):
+        self.tasks.clear()
+        self._next_id = 1
+
+    # ToDo => Function to remove a task by ID
+    def remove_task(self, task_id):
+        # write your code here
+        pass
 
 
-# ToDo => Function to remove a task by ID
-def remove_task(task_id):
-    # write your code here
-    pass
+    # ToDo => Function to mark a task as completed
+    def complete_task(self, task_id):
+        # write your code here
+        pass
 
 
-# ToDo => Function to mark a task as completed
-def complete_task(task_id):
-    # write your code here
-    pass
+    # ToDo => Function to list all tasks
+    def list_tasks(self):
+        # write your code here
+        pass
 
 
-# ToDo => Function to list all tasks
-def list_tasks():
-    # write your code here
-    pass
-
-
-# ToDo => Function to edit a task
-def edit_task(task_id, field, new_value):
-    # write your code here
-    pass
+    # ToDo => Function to edit a task
+    def edit_task(self, task_id, field, new_value):
+        # write your code here
+        pass

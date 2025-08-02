457826c9be41365633d982299dc3dffadc980713
Walaa <walaa@Walaas-MacBook-Air.local>
2025-08-02 17:08:10 +0300

    fix test function

--- Remove_Function.py
+++
@@ -1,7 +0,0 @@
-def remove_task(task_id):
-    for task in tasks:
-        if task["task_id"] == task_id:
-            tasks.remove(task)
-            print(f"Task with ID {task_id} removed successfully!")
-            return
-    raise ValueError(f"Task with ID {task_id} not found.")
